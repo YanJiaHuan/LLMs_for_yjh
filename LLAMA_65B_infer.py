61c0dde573fa0c671370c0ae86364920ddaca615
William yan <1337842463@qq.com>
2023-07-25 13:54:30 +0800

    updates

--- LLAMA_65B_infer.py
+++ LLAMA_65B_infer.py
@@ -93,21 +93,6 @@ ds_config = {
     "num_different_global_patterns": 4
   }
 }
-@dataclass
-class GenerationConfig:
-    max_new_tokens: Optional[int] = field(default=256)
-    min_new_tokens: Optional[int] = field(default=None)
-    do_sample: Optional[bool] = field(default=False)
-    num_beams: Optional[int] = field(default=1)
-    penalty_alpha: Optional[float] = field(default=True)
-    temperature: Optional[float] = field(default=1.0)
-    top_k: Optional[int] = field(default=10)
-    top_p: Optional[float] = field(default=1.0)
-    typical_p: Optional[float] = field(default=1.0)
-    diversity_penalty: Optional[float] = field(default=0.0)
-    reptition_penalty: Optional[float] = field(default=1.0)
-    length_penalty: Optional[float] = field(default=1.0)
-    no_repeat_ngram_size: Optional[int] = field(default=0)
 
 # must run before instantiating the model
 # ds_config is deepspeed config object or path to the file
@@ -133,9 +118,27 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 text = "What is the average enrollment of schools?\ndb_id:school_player\nTable player, columns = [*,Player_ID,Player,Team,Age,Position,School_ID]\nTable school, columns = [*,School_ID,School,Location,Enrollment,Founded,Denomination,Boys_or_Girls,Day_or_Boarding,Year_Entered_Competition,School_Colors]\nTable school_details, columns = [*,School_ID,Nickname,Colors,League,Class,Division]\nTable school_performance, columns = [*,School_Id,School_Year,Class_A,Class_AA]\n\nforeign key:[school_details.School_ID = school.School_ID,school_performance.School_Id = school.School_ID,player.School_ID = school.School_ID]\nprimary key:[school.School_ID,school_details.School_ID,school_performance.School_Id,player.Player_ID]\n', 'Find the first names of the teachers that teach first grade.\ndb_id:student_1\nTable list, columns = [*,LastName,FirstName,Grade,Classroom]\nTable teachers, columns = [*,LastName,FirstName,Classroom]\n\nforeign key:]\nprimary key:[list.LastName,teachers.LastName]\n"
+# text = "This is a test sentence, what are you?"
 inputs = tokenizer.encode(text, return_tensors="pt").to(device=local_rank)
 
-outputs = model.generate(input_ids=inputs, max_new_tokens=20)
+@dataclass
+class GenerationConfig:
+    max_new_tokens: Optional[int] = field(default=64)
+    min_new_tokens: Optional[int] = field(default=None)
+    do_sample: Optional[bool] = field(default=False)
+    num_beams: Optional[int] = field(default=1)
+    penalty_alpha: Optional[float] = field(default=True)
+    temperature: Optional[float] = field(default=1.0)
+    top_k: Optional[int] = field(default=10)
+    top_p: Optional[float] = field(default=1.0)
+    typical_p: Optional[float] = field(default=1.0)
+    diversity_penalty: Optional[float] = field(default=0.0)
+    reptition_penalty: Optional[float] = field(default=1.0)
+    length_penalty: Optional[float] = field(default=1.0)
+    no_repeat_ngram_size: Optional[int] = field(default=0)
+
+
+outputs = model.generate(input_ids=inputs, generation_config=GenerationConfig())
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 # deepspeed --num_gpus 8 LLAMA_65B_infer.py
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python LLAMA_65B_infer.py
