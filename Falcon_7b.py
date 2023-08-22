5ea6c7c2cbdc7b117f02f5f5d6a8cf810883e1b0
William yan <1337842463@qq.com>
2023-08-22 12:03:07 +0800

    updates

--- Falcon_7b.py
+++ Falcon_7b.py
@@ -14,7 +14,7 @@ from peft import (
     LoraConfig,
     get_peft_model
 )
-from datasets import Dataset
+from datasets import Dataset, load_dataset, load_metric
 # model_name
 model_id = "tiiuae/falcon-7b"
 
@@ -35,84 +35,83 @@ lora_config = LoraConfig(
 )
 
 # load model and tokenizer
-device = 'cuda'
+
+
 tokenizer = AutoTokenizer.from_pretrained(
     model_id,
     padding_side="right",
     use_fast=False,
 )
-model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config, device_map=device, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=nf4_config, trust_remote_code=True)
 model.gradient_checkpointing_enable()
 model = prepare_model_for_kbit_training(model)
 model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
 
+
 # load data
 Data_path = "./Data/1890/background_train.json"
-data = pd.read_json(Data_path, lines=True)
-train_dataset = data.sample(frac = 0.8, random_state=42)
-test_dataset = data.drop(train_dataset.index).reset_index(drop=True)
-train_dataset = Dataset.from_pandas(train_dataset)
-test_dataset = Dataset.from_pandas(test_dataset)
-def preprocess_function(example, tokenizer):
-    instruction = example['instruction']
-    input_text = example['input']
-    output_text = example['output']
+data = load_dataset('json', data_files=Data_path)
+data = data['train'].train_test_split(test_size=0.1)
 
-    # Concatenate instruction and input with special tokens for separation
-    instruction_str = f"Instruction: {instruction}"
-    input_str = f"Context: {input_text}"
+def preprocess_function(examples):
+    inputs = ["### Instruction:\n" + instruction + "\n\n" + "### Input:\n" + context + "\n\n" for instruction, context in
+              zip(examples["instruction"], examples["input"])]
+    model_inputs = tokenizer(inputs, padding="max_length", max_length=1024, truncation=True)
+    labels = tokenizer(examples["output"], padding="max_length", max_length=1024, truncation=True)
 
-    # Tokenize the concatenated string
-    tokenized_input = tokenizer(instruction_str + " " + input_str, return_tensors="pt", padding=True, truncation=True)
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
-    # Tokenize the output string
-    tokenized_output = tokenizer(output_text, return_tensors="pt", padding=True, truncation=True)["input_ids"]
+tokenized_data = data.map(preprocess_function, batched=True)
 
-    return {
-        "input_ids": tokenized_input["input_ids"].squeeze(0),  # Remove batch dimension
-        "attention_mask": tokenized_input["attention_mask"].squeeze(0),  # Remove batch dimension
-        "labels": tokenized_output.squeeze(0)  # Remove batch dimension
-    }
+# load metric
+bleu_metric = load_metric('sacrebleu')
 
-train_dataset = train_dataset.map(lambda e: preprocess_function(e, tokenizer), batched=True)
-test_dataset = train_dataset.map(lambda e: preprocess_function(e, tokenizer), batched=True)
+def compute_metrics(eval_preds):
+    labels = eval_preds.label_ids
+    preds = eval_preds.predictions
+
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    decoded_labels = [tokenizer.batch_decode(label, skip_special_tokens=True) for label in labels]
+    print(decoded_preds[:5])
+    result = bleu_metric.compute(predictions=decoded_preds, references=decoded_labels)
+    return {"bleu_score": result['score']}  # Rename the score as 'bleu_score'
 
 # train
 
-trainer = transformers.Seq2SeqTrainer(
-    model=model,
-    train_dataset=train_dataset,
-    eval_dataset=test_dataset,
-    args=transformers.Seq2SeqTrainingArguments(
-        logging_dir="./logs_for_falcon_7b" ,     # Path to directory to save logs
-        logging_strategy='steps',   # Log after every X steps
-        logging_steps=10,           # Set X to be 100
-        output_dir="./Checkpoints/falcon/1890",
-        num_train_epochs=10,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=32,
-        gradient_accumulation_steps=4,
-        gradient_checkpointing=True,
-        warmup_ratio=0.03,
-        group_by_length=False,
-        lr_scheduler_type="cosine",
-        evaluation_strategy="steps",  # Change evaluation_strategy to "steps"
-        save_strategy="steps",
-        eval_steps=50,
-        save_steps=100,# Add eval_steps parameter need to lower the log/eval/save steps to see the report results
-        learning_rate=5e-4,
-        fp16=False,
-        optim="paged_adamw_8bit",
-        predict_with_generate=True,
-        generation_num_beams=4,
-        generation_max_length=513,
-        include_inputs_for_metrics=True,
-        # deepspeed=ds_config,
-    ),
+training_args = Seq2SeqTrainingArguments(
+    logging_dir="./logs_for_falcon_7b",  # Path to directory to save logs
+    output_dir="./Checkpoints/falcon/1890",
+    evaluation_strategy="steps",
+    eval_steps=20,
+    learning_rate=1e-4,
+    weight_decay=1e-5,
+    save_strategy='steps',
+    save_steps=600,
+    num_train_epochs=500,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=1,
+    eval_accumulation_steps=1,
+    fp16=True,
+    predict_with_generate=True,
+    logging_strategy='steps',   # Log after every X steps
+    logging_steps=100           # Set X to be 100
 )
-model.config.use_cache = True  # silence the warnings. Please re-enable for inference!
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_args,
+    train_dataset=tokenized_data['train'],
+    eval_dataset=tokenized_data['test'],
+    compute_metrics=compute_metrics,
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 trainer.train()
 
 
